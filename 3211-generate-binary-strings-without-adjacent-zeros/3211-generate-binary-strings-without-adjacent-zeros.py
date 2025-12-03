7fe29e9f46f927fa642846c0fae1db7c1e55fb5e
Rishit Srivastava <rishit.23scse1011922@galgotiasuniversity.ac.in>
2025-12-03 12:15:56 +0530

    Time: 117 ms (21.05%), Space: 21.2 MB (8.77%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
@@ -1,23 +1,8 @@
 class Solution(object):
     def validStrings(self, n):
-        ans=[]
-        if n==1:
-            return ["0","1"]
+        ans = []
         for i in range(2**n):
-            if i==0:
-                continue
-            if i==2:
-                a="10"
-                if len(a)==n-1:
-                    b="0"
-                    a=b+a
-                
-                    ans.append(a)
-            elif (2**n)%i!=0:
-                a=str(bin(i))[2:]
-                if len(a)==n-1:
-                    b="0"
-                    a=b+a
-                if len(a)==n:
-                    ans.append(a)
+            s = bin(i)[2:].zfill(n)   
+            if "00" not in s:       
+                ans.append(s)
         return ans
