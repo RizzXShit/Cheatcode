dc90d896e8ff5a73034728e520fa672c660c55de
Rishit Srivastava <rishit.23scse1011922@galgotiasuniversity.ac.in>
2025-12-04 10:14:10 +0530

    Time: 3 ms (99.72%), Space: 12.5 MB (10.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def fib(self, n):
+    '''def fib(self, n):
         dp=[-1]*(n+1)
         return self.solve(n,dp)
         
@@ -11,6 +11,22 @@ class Solution(object):
         if dp[n]!=-1:
             return dp[n]
         dp[n]=self.solve(n-1,dp)+self.solve(n-2,dp)
+        return dp[n]'''
+    def fib(self,n):
+        dp=[-1]*(n+1)
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.solve(n,dp)
+        
+              
+        
+    def solve(self,n,dp):
+        dp[0]=0
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
     
     
