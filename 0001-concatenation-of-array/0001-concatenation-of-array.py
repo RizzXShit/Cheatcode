346abc99bb024ffad3f943f8bd8884aabc7941c4
Rishit Srivastava <rishit.23scse1011922@galgotiasuniversity.ac.in>
2025-12-09 11:31:19 +0530

    Time: 0 ms (100%), Space: 12.6 MB (92.01%) - LeetHub

--- 0001-concatenation-of-array/0001-concatenation-of-array.py
+++ 0001-concatenation-of-array/0001-concatenation-of-array.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def getConcatenation(self, nums):
-        a=[]
-        for i in range(0,len(nums)):
-            a.append(nums[i])
-        for j in a:
-            nums.append(j)
-        return nums
+        
+        return nums+nums
         
