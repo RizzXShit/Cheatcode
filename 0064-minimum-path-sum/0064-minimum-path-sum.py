3b0fb7662db539016e4e6382805258a735649e0d
Rishit Srivastava <rishit.23scse1011922@galgotiasuniversity.ac.in>
2025-12-11 10:17:25 +0530

    Time: 14 ms (77.65%), Space: 14.2 MB (51.44%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,8 +2,8 @@ class Solution(object):
     def minPathSum(self, grid):
         m=len(grid)
         n=len(grid[0])
-        dp=[[-1]*n for _ in range(m)]
-        def minval(i,j):
+        dp=[[0]*n for _ in range(m)]
+        '''def minval(i,j):
             if i<0 or j<0:
                 return float('inf')
             if i==0 and j==0:
@@ -14,7 +14,17 @@ class Solution(object):
             a= grid[i][j]+min(minval(i-1,j),minval(i,j-1))
             dp[i][j]=a
             return a
-        return minval(m-1,n-1)
+        return minval(m-1,n-1)'''
+        dp[0][0]=grid[0][0]
+        for i in range(1,n):
+            dp[0][i]=dp[0][i-1]+grid[0][i]
+        for j in range(1,m):
+            dp[j][0]=dp[j-1][0]+grid[j][0]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1])
+        return dp[m-1][n-1]
+
 
             
 
