ccba2384560121996b1905c8079a6c0ea22623a7
Rishit Srivastava <rishit.23scse1011922@galgotiasuniversity.ac.in>
2025-12-24 09:55:42 +0530

    Time: 575 ms (65.49%), Space: 28.4 MB (11.5%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -2,10 +2,10 @@ class Solution(object):
     def minInsertions(self, s):
         i=0
         j=len(s)-1
-        memo={}
+        memo=[[-1]*(j+1) for _ in range(j+1)]
         def solve(i,j):
-            if (i,j) in memo:
-                return memo[(i,j)]
+            if memo[i][j]!=-1:
+                return memo[i][j]
             if i>=j:
                 return 0
             if s[i]==s[j]:
@@ -13,6 +13,7 @@ class Solution(object):
             
             else:
                 a= min(1+solve(i+1,j),1+solve(i,j-1))
-                memo[(i,j)]=a
+                memo[i][j]=a
                 return a
         return solve(i,j)
+        
