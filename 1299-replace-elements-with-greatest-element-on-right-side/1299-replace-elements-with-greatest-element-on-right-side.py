97519dce4222e800942765a9e5f5b5b2a6660034
Rishit Srivastava <rishit.23scse1011922@galgotiasuniversity.ac.in>
2026-02-11 22:12:23 +0530

    Time: 24 ms (83.01%), Space: 13.8 MB (87.75%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        n = len(arr)
+        max_right = -1
+        for i in range(n-1, -1, -1):
+            temp = arr[i]
+            arr[i] = max_right
+            if temp > max_right:
+                max_right = temp
+        return arr
