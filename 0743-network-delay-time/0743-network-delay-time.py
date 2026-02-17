4b1a9c9ef8f3350ad0080a4253b42d57e483d43d
Rishit Srivastava <rishit.23scse1011922@galgotiasuniversity.ac.in>
2026-02-17 12:08:33 +0530

    Time: 342 ms (95.33%), Space: 14.7 MB (55.66%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,22 @@
+import heapq
+
+class Solution(object):
+    def networkDelayTime(self,times, n, k):
+        g = [[] for _ in range(n+1)]
+        for u,v,w in times:
+            g[u].append((v,w))
+        dist = [float('inf')] * (n+1)
+        dist[k] = 0
+        h = [(0,k)]
+        while h:
+            d,u = heapq.heappop(h)
+            if d > dist[u]: continue
+            for v,w in g[u]:
+                if dist[v] > d+w:
+                    dist[v] = d+w
+                    heapq.heappush(h,(dist[v],v))
+        ans = max(dist[1:])
+        if ans==float('inf'):
+            return -1
+        else:
+            return ans
